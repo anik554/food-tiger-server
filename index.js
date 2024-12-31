0afae17c937c9c83e6546e9dfe6f01a08423e7fc
anik924 <anikimtiaz924@gmail.com>
2024-12-31 21:53:09 +0600

    server setup and get featured food api create

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Job is falling from the sky");
+});
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@movies.yat9m.mongodb.net/?retryWrites=true&w=majority&appName=Movies`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const featuredFoodCollection = client
+      .db("foodTiger")
+      .collection("foods");
+
+    app.get("/featuredFood", async (req, res) => {
+      const cursor = featuredFoodCollection.find();
+      const result = await cursor.toArray();
+      console.log("result",result)
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
