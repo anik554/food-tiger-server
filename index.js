7555bd53ebfb0bd44efb2337e05a2c930beec025
anik924 <anikimtiaz924@gmail.com>
2025-01-01 01:08:43 +0600

    get availableFoods details api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -41,11 +41,18 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/addFood",async(req,res)=>{
-      const cursor = addFoodCollection.find()
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+    app.get("/availableFoods", async (req, res) => {
+      const cursor = addFoodCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/availableFood/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await addFoodCollection.findOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
